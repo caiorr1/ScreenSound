44bcbf65f8561f24832bcbb04acbdd010f105f5f
caiorr1 <caiorrodrigues2004@gmail.com>
2023-10-13 13:03:33 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ScreenSound
+﻿string mensagemBoasVindas = "\nBoas Vindas ao Screen Sound";
+
+
+void ExibirMensagemBoasVindas()
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemBoasVindas);
 }
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\n1 - Para registrar uma banda");
+    Console.WriteLine("2 - Para mostrar todas as bandas");
+    Console.WriteLine("3 - Para avaliar uma banda");
+    Console.WriteLine("4 - Para exibir a média de uma banda");
+    Console.WriteLine("0 - Para sair");
+}
+
+ExibirMensagemBoasVindas();
+ExibirOpcoesDoMenu();
